09d0def6eaf71dd8d9d29bcd5b8b04459d6069c9
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:24:56 +0100

    Add project files.

---
+++ Array Inlämning 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal ska du skriva");
+            int tal = int.Parse(Console.ReadLine());
+            string[] talen = new string[tal];
+
+            for (int i = 0; i < tal; i++)
+            {
+                Console.WriteLine("Skriv in talen");
+                talen[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Här är talen du skrev");
+            for (int i = 0; i < tal; i++)
+            {
+                Console.WriteLine(talen[i]);
+            }
+        }
+    }
+}
